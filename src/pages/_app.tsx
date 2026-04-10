3e9db6c7471dd2ac29f41f913de9c62f81d9962c
Stephanie Couto <stephanie@example.com>
2026-04-10 16:02:40 -0300

    efeitos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,27 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import "../styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-81LVHQV2Q3"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-81LVHQV2Q3');
+            `,
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
