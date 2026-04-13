b0a95eb8445cd8abc9d8c6b66b805e3ed7ede222
Stephanie Couto <stephanie@example.com>
2026-04-13 15:39:50 -0300

    icone na guia do navegador

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,10 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
+        {/* Favicon com ícone de coração */}
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='75' font-size='79'>❤️</text></svg>" />
+        <title>Momback Doces</title>
+        
         {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-81LVHQV2Q3"></script>
         <script
